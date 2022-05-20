c2a82fd0710fb29d25d3cd8343c9c42e3ab9aff6
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2022-05-19 21:32:48 -0500

    Updated About section
    There's a data fetching error from Sanity that needs to be fixed.

--- portofolio-frontend/src/container/About/About.jsx
+++ portofolio-frontend/src/container/About/About.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 
 import './About.scss';
 
 import { motion } from 'framer-motion';
 import { Images } from '../../constants';
 
-const About = () => {
-  return (
-    <div className='app__about app__flex'>
+import { urlFor, client } from '../../Sanity_client';
 
-    </div>
+const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => { 
+      console.log(data)
+    })
+
+  }, [])
+  
+
+  return (
+    <>
+      <h2 className="head-text"> Think it <span> carefully </span><br /> implement it <span> Faithfully </span>
+      </h2>
+
+      <div className='app__profiles'>
+        {
+          abouts.map((about, index) => (
+            <motion.div
+              whileInView= {{ opacity: 1}}
+              whileHover= {{ scale: 1.1 }}
+              transition= {{ duration: 0.3, type: 'tween'}}
+              className= 'app__profile-item'
+              key={ about.title + index }
+            >
+              <img src= {about.imgURL} alt= { about.title } />
+              <h2 className= 'bold-text' style={{ marginTop: 20}}> { about.title } </h2>
+              <p className= 'p-text' style={{ marginTop: 10}}> { about.description } </p>
+
+            </motion.div>
+           ) )
+        }
+      </div>
+    </>
   )
 }
 
