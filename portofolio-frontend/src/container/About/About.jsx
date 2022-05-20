1906f4a2a6da7e4a2e92780eac73c2ecd85190e4
tibucuartas <tibucuartas@gmail.com>
2022-05-19 19:00:57 -0500

    Started implementing the About section
    Updated the node modules so the application could run on the local machine, and briefly started implementing the About section

--- portofolio-frontend/src/container/About/About.jsx
+++ portofolio-frontend/src/container/About/About.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+
 import './About.scss';
+
+import { motion } from 'framer-motion';
+import { Images } from '../../constants';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='app__about app__flex'>
+
+    </div>
   )
 }
 
